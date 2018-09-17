b3137775054917a6bbab68d5802f98bb72ddea9e
Smirnov Oleg <sm-oleg1209@yandex.ru>
2018-09-18 00:31:37 +0300

    Task#1

---
+++ Smirnov Oleg/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define sqr(a) (a) * (a)
+
+int main() {
+    int sum[28];
+    for (int i = 0; i < 28; ++i)
+        sum[i] = 0;
+    for (int a = 0; a < 10; ++a)
+        for (int b = 0; b < 10; ++b)
+            for (int c = 0; c < 10; ++c)
+                sum[a + b + c]++;
+    int ans = 0;
+    for (int i = 0; i < 28; ++i)
+        ans += sqr(sum[i]);
+    printf("%d", ans);
+}
