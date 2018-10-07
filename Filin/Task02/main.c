4b5f3270e9c974d3ce4507bf01c304e09b4ac889
ASUS-ПК\Asus <alexfilin2000@gmail.com>
2018-10-07 20:46:18 +0300

    Task02

---
+++ Filin/Task02/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+extern int sortirovka (int razmer, int massive[]);
+
+int main(int argc, char* argv[])
+{
+    int from = -2147483647, to = 2147483647, i = 1;
+  while(i < argc){
+        if (argv[i][2] == 'f'){
+            from = atoi (argv[i + 2]);
+            i = i + 3;
+             continue;
+        }
+        if (argv[i][2] == 't') {
+            to = atoi (argv[i+2]);
+            i = i + 3;
+            continue;
+        }
+        i++;
+    }
+
+    int j, res, count = 0, mas[100]={0};
+    i = 0;
+    char c;
+    c = getchar();
+    while (c != '\n'){
+       if (c == ' '){
+        count++;
+        c = getchar();
+        continue;
+       }
+       if (c >= '0' && c <= '9'){
+        mas[count] = mas [count]*10 + c - '0';
+        c = getchar();
+        continue;
+       }
+       if (c == '-'){
+            c = getchar();
+        while (c >= '0' && c <= '9'){
+        mas[count] = mas[count]*10 - (c - '0');
+        c = getchar();
+       }
+       continue;
+       }
+    }
+
+    count++;
+    int sort[100];
+    i = 0;
+    j = 0;
+      while(i < count){
+    if (mas[i] <= from) {
+       fprintf(stdout, "%d ", mas[i]);
+       i++;
+       continue;}
+    if (mas[i] >= to) {
+        fprintf(stderr, "%d ", mas[i]);
+        i++;
+        continue;}
+    sort[j] = mas[i];
+    j++;
+    i++;
+  }
+    res = sortirovka (j, sort);
+    printf("\n%d",res);
+
+    return 0;
+}
