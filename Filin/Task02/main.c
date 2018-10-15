100deb00262c0aca64ec90742ce8dab11bc79db3
ASUS-ПК\Asus <alexfilin2000@gmail.com>
2018-10-15 18:02:47 +0300

    Problems with space

--- Filin/Task02/main.c
+++ Filin/Task02/main.c
@@ -7,19 +7,43 @@ extern int sortirovka (int razmer, int massive[]);
 int main(int argc, char* argv[])
 {
     int from = -2147483647, to = 2147483647, i = 1;
-  while(i < argc){
-        if (argv[i][2] == 'f'){
-            from = atoi (argv[i + 2]);
-            i = i + 3;
-             continue;
-        }
-        if (argv[i][2] == 't') {
-            to = atoi (argv[i+2]);
-            i = i + 3;
-            continue;
-        }
-        i++;
-    }
+while (i < argc) {
+		if(argv[i][2] == 'f') {
+			if(argv[i][6] == '=') {
+				if(argv[i][7] == '\0') {
+					++i;
+					from = atoi(argv[i]);
+				} else {
+					from = atoi(argv[i] + 7);
+				}
+			} else {
+				++i;
+				if(argv[i][1] == '\0') {
+					++i;
+					from = atoi(argv[i]);
+				} else {
+					from = atoi(argv[i] + 1);
+				}
+			}
+		} if(argv[i][2] == 't'){
+			if(argv[i][4] == '=') {
+				if(argv[i][5] == '\0') {
+					++i;
+					to = atoi(argv[i]);
+				} else {
+					to = atoi(argv[i] + 5);
+				}
+			} else {
+				++i;
+				if(argv[i][1] == '\0') {
+					++i;
+					to = atoi(argv[i]);
+				} else {
+					to = atoi(argv[i] + 1);
+				}
+			}
+		}
+	i++;}
 
     int j, res, count = 0, mas[100]={0};
     i = 0;
