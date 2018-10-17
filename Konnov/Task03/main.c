faa19760f85e68aa8b50eb019da75342d9179d5e
Юля Иванова <k.kirialosk@gmail.com>
2018-10-17 12:50:07 +0300

    no message

--- Konnov/Task03/main.c
+++ Konnov/Task03/main.c
@@ -5,10 +5,10 @@ int N;
 double sum_series(int Num)
 {
 	int i;
-	double retu = 0;
-	for(i = 1; i <= Num; i++)
+	double retu = 0.0;
+	for(i = Num; i < 2 * Num; i++)
 	{
-		retu += -1 / (double) ((2 * i - 1) * 2 * i);
+		retu += -1.0 / (double)i;
 	}
 	return retu;
 }
@@ -16,6 +16,6 @@ double sum_series(int Num)
 double main()
 {
 	scanf("%d", &N);
-	printf("%f", sum_series(N) );
+	printf("%.10lf", sum_series(N));
 	return sum_series(N);
 }
