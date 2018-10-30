04877aee1c8ecf3969e7432c88d484c877679cb8
Lev Malets <lk.malets30@gmail.com>
2018-10-31 01:40:49 +0300

    Task 03

--- Malets/Task03/main.c
+++ Malets/Task03/main.c
@@ -2,21 +2,15 @@
 
 double sum_series( int N )
 {
-	int64_t width = 1;
-	while ((N >> width) > 0)
-		width++;
-	width--;
-
-	int64_t count = 1 << width;
-	int64_t denumerator = count << 1;
+	int64_t denumerator = N << 1;
 	int64_t numerator = 2;
 
 	double result = 0;
-	while (count > 0)
+	while (N > 0)
 	{
 		result -= (double)numerator / (denumerator + 1);
 		denumerator += numerator;
-		count--;
+		N--;
 	}
 
 	return result;
