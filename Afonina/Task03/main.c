9ae1d7a828bebdf53468e62437a7329aa6199e77
Afonina Olga <o.aphonina@gmail.com>
2018-10-31 00:44:48 +0300

    Task03

--- Afonina/Task03/main.c
+++ Afonina/Task03/main.c
@@ -2,9 +2,8 @@
 
 long double sum_series(int n) {
     long double sum = -1;
-    int cnt;
-    for (int i = 2; i < 2 * n; i += 2, cnt = i) {
-        sum += (long double)1 / (i * (i + 1));
+    for (int i = 2; i < 2 * n; i += 2) {
+        sum += (long double)1 / ((long double)i * (i + 1));
     }
     return sum;
 }
@@ -12,6 +11,6 @@ long double sum_series(int n) {
 int main() {
     int n;
     scanf("%d", &n);
-    printf("%.19Lf\n", sum_series(n));
+    printf("%.19Lf", sum_series(n));
 }
 
