c9af5658ec24a1ea2f55f4dc6ca1e6fcc33b6498
HappyLittleNurgling <Skorpilesha@yandex.ru>
2018-10-31 10:06:01 +0300

    Task03 update

--- Privalihin/Task03/main.c
+++ Privalihin/Task03/main.c
@@ -2,12 +2,14 @@
 
 long double sum_series(int N)
 {
-    long double res = 0;
+    long double res = (long double)0.0;
+    long double j = (long double)N + 1.0;
     for (int i = N + 1; i <= 2 * N; i++)
     {
-        res = res + 1.0 / ((long double)i);
+        res -= 1.0 / j;
+        j = j + 1.0;
     }
-    return -res;
+    return res;
 
 }
 
@@ -15,5 +17,5 @@ int main()
 {
     int N;
     scanf("%d", &N);
-    printf("%Lf \n", sum_series(N));
+    printf("%.18Lf \n", sum_series(N));
 }
