63334ff3726a5a2db1d026929eaec146b093eb33
Tsirikov <tsirikov.s.a@gmail.com>
2018-11-05 16:51:31 +0300

    1) Better reads parametrs. 2) Uses calloc() and free(). 3) Doesn`t use <limits.h>.

--- Tsirikov/Task02/main.c
+++ Tsirikov/Task02/main.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <ctype.h>
 
 extern int __sort(int counter, int massive[]);
 
+//optional "from" and "to"
+int down = NULL;
+int high = NULL;
+
+void params(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
-    int down = INT_MIN, high = INT_MAX;
-    for (int params = 1; params < (argc - 1); params += 3)
+    params(argc, argv);
+    //read and filter massive
+    int *massive = calloc(100, sizeof(int));
+    int counter = 0;
+    while (scanf("%d", massive + counter) == 1)
     {
-        if (argv[params][2] == 'f')
-        {
-            down = atoi(argv[params + 2]);
-        }
-        else
-        {
-            high = atoi(argv[params + 2]);
-        }
-    }
-    //reading parametrs
-    int massive[100];
-    for (int i=0; i < 100; i++)
-    {
-        massive[i] = 0;
-    }
-    //making massive
-    int counter=0;
-    while (scanf("%d", (massive + counter)) == 1)
-    {
-        if (massive[counter] <= down)
+        if ((down != '\0') && (massive[counter] <= down))
         {
             fprintf(stdout, "%d ", massive[counter]);
             massive[counter] = 0;
             counter--;
         }
-        else if (massive[counter] >= high)
+        else if ((high != '\0') && (massive[counter] >= high))
         {
             fprintf(stderr, "%d ", massive[counter]);
             massive[counter] = 0;
@@ -42,8 +32,42 @@ int main(int argc, char *argv[])
         }
         counter++;
     }
-    //reading massive
+    //sort massive
     int counterOfTranspositions = __sort(counter, massive);
-    //goto sort.s
+    free(massive);
     return counterOfTranspositions;
 }
+
+//read parametrs
+void params(int argc, char *argv[])
+    {
+        char *parametrs = calloc(40, sizeof(char));
+        int counter = 0;
+        for (int i = 1, j = 0; i <= argc - 1; i++, j = 0)
+    	    while ((argv[i])[j] != '\0')
+    		    parametrs[counter++] = (argv[i])[j++];
+        parametrs[counter] = '\0';
+        counter = 0;
+        while (parametrs[counter++] != '\0')
+    	    if (parametrs[counter] == 'f')
+    	    {
+    		    counter += 5;
+    		    int sign = parametrs[counter] == '-' ? -1 : 1;
+    		    if (sign == -1) ++counter;
+    		    down = 0;
+    		    while(isdigit(parametrs[counter]))
+    			    down = down * 10 + parametrs[counter++] - '0';
+    		    down *= sign;
+    	    }
+            else if (parametrs[counter] == 't')
+    	    {
+    		    counter += 3;
+    		    int sign = parametrs[counter] == '-' ? -1 : 1;
+    		    if (sign == -1) ++counter;
+    		    high = 0;
+    		    while (isdigit(parametrs[counter]))
+    			    high = high * 10 + parametrs[counter++] - '0';
+    		    high *= sign;
+    	    }
+        free(parametrs);
+    }
