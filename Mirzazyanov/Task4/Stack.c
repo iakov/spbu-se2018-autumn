8302d1ec2bf9a47c2282df04ff76fcbf1d7974fb
Gleb.Mirzazyanov <gleb.mirzazyanov@gmail.com>
2018-11-21 12:05:20 +0300

    Task_04

---
+++ Mirzazyanov/Task4/Stack.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void other() {
+	printf("Congratulation! You are Hacker!\n");
+}
+void input() {
+	char buffer[10];
+	printf("Destroy me: \n");
+	scanf("%s", buffer);
+	printf("Ahaha, no time yet! %s!\n", buffer);
+}
+int main() {
+	printf("Let's try it!)\n");
+	input();
+	return 0;
+}
