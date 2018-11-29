197bbee74e18e99ad8546b47d776bd1a42556986
Tsirikov <tsirikov.s.a@gmail.com>
2018-11-29 05:08:57 +0300

    Remake of main.c. Reading until 'EOF' or 100th suitable element.

--- Tsirikov/Task02/main.c
+++ Tsirikov/Task02/main.c
@@ -1,76 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-extern int __sort(int counter, int massive[]);
+char IsDown = 0, IsHigh = 0;
+int Down, High;
 
-//optional "from" and "to"
-int down = NULL;
-int high = NULL;
-
-void params(int argc, char *argv[]);
+int read_parametrs(int, char **);
+int read_massive(int *);
+extern int __sort(int, int*);
 
 int main(int argc, char *argv[])
 {
-    params(argc, argv);
-    //read and filter massive
-    int *massive = calloc(100, sizeof(int));
-    int counter = 0;
-    while (scanf("%d", massive + counter) == 1)
-    {
-        if ((down != '\0') && (massive[counter] <= down))
-        {
-            fprintf(stdout, "%d ", massive[counter]);
-            massive[counter] = 0;
-            counter--;
-        }
-        else if ((high != '\0') && (massive[counter] >= high))
-        {
-            fprintf(stderr, "%d ", massive[counter]);
-            massive[counter] = 0;
-            counter--;
-        }
-        counter++;
-    }
-    //sort massive
-    int counterOfTranspositions = __sort(counter, massive);
-    free(massive);
-    return counterOfTranspositions;
+	read_parametrs(argc, argv);
+	int *massive = calloc(100, sizeof(int));
+	int AmountOfElements = read_massive(massive);
+	int Transpositions = __sort(AmountOfElements, massive);
+	free(massive);
+	return Transpositions;
 }
 
-//read parametrs
-void params(int argc, char *argv[])
-    {
-        char *parametrs = calloc(40, sizeof(char));
-        int counter = 0;
-        for (int i = 1, j = 0; i <= argc - 1; i++, j = 0)
-    	    while ((argv[i])[j] != '\0')
-    		    parametrs[counter++] = (argv[i])[j++];
-        parametrs[counter] = '\0';
-        counter = 0;
-        int *lim;
-        while (parametrs[counter++] != '\0')
-            if (parametrs[counter] == 'f' || parametrs[counter] == 't')
-                {
-                    /* '3' and '5' are numbers of symbols between
-                    'f' or 't' and first digit or minus in string*/
-                    if (parametrs[counter] == 'f')
-                    {
-                        counter += 5;
-                        lim = &down;
-                    }
-                    else
-                    {
-                        counter += 3;
-                        lim = &high;
-                    }
-        		    int sign = parametrs[counter] == '-' ? -1 : 1;
-        		    if (sign == -1)
-                        ++counter;
-        		    *lim = 0;
-        		    while(isdigit(parametrs[counter]))
-        			    *lim = *lim * 10 + parametrs[counter++] - '0';
-        		    *lim *= sign;
-        	    }
-        free(parametrs);
-    }
+
+int read_parametrs(int argc, char *argv[])
+{
+	char CharBuf;
+	int NumberBuf = 0;
+	for (int i = 1, j = 0; i < argc; i++, j = 0)
+		while ( (CharBuf = argv[i][j++]) != '\0' )
+			if ( (IsDown == 1 || IsHigh == 1) && (CharBuf == '-' || (CharBuf >= '0' && CharBuf <= '9')) )
+			{
+				NumberBuf = CharBuf == '-' ? (int) (argv[i][j++] - '0') : (int) (CharBuf - '0');
+				while (argv[i][j] != '\0')
+					
+					NumberBuf = NumberBuf * 10 + (int) (argv[i][j++] - '0');
+				
+				if ( CharBuf =='-' )
+
+					NumberBuf *= -1;
+				
+				if (IsDown == 1)
+				{
+					Down = NumberBuf;
+					IsDown++;
+				}
+				else
+				{
+					High = NumberBuf;
+					IsHigh++;
+				}
+			}
+			else switch (CharBuf)
+			{
+				case 'f':
+					IsDown++;
+					break;
+				case 't':
+					IsHigh++;
+					break;
+				default:
+					break;
+			}
+	return 0;
+}
+
+
+int read_massive(int *massive)
+{
+	int Buf1 = getchar(), Buf0 = '\0';
+	int Counter = 0, Sign = 1;
+	while ( Buf0 != EOF && Counter < 100)
+	{
+		if ( Buf1 == '-' )
+			
+			Sign = -1;
+
+		else if ( Buf1 >= '0' && Buf1 <= '9' )
+		
+			massive[Counter] = massive[Counter] * 10 + (int) (Buf1 - '0');
+		
+		else if ( Buf0 >= '0' && Buf0 <= '9' )
+		{
+			massive[Counter] *= Sign;
+			if (IsDown != 0 && massive[Counter] <= Down)
+			
+				fprintf(stdout, "%d ", massive[Counter]);
+			
+			if (IsHigh != 0 && massive[Counter] >= High)
+			
+				fprintf(stderr, "%d ", massive[Counter]);
+			
+			if ( !(IsDown != 0 && massive[Counter] <= Down) && !(IsHigh != 0 && massive[Counter] >= High) )
+			
+				Counter++;
+
+			else
+
+				massive[Counter] = 0;
+
+			Sign = 1;
+		}
+		Buf0 = Buf1;
+		Buf1 = getchar();
+	}
+	return Counter;
+}
