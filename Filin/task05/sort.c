48d254cf00dae3076d8640a35e3038431b7eae26
ASUS-ПК\Asus <alexfilin2000@gmail.com>
2018-12-05 18:07:47 +0300

    no message

--- Filin/task05/sort.c
+++ Filin/task05/sort.c
@@ -4,114 +4,146 @@
 
 char* string[100000000];
 
-void bubblesort(char **string, int n){
-int i, j;
-for (i = 0; i < n; i++){
-    for (j = 0; j < n - i - 1; j++){
-        if (strcmp(string[j], string[j + 1]) == 1) {
-                char *buf = string[j];
-                string[j] = string[j + 1];
-                string[j + 1] = buf;
-    }}}}
-
-void insertionsort(char **string, int n){
-    int i, j;
-    for (i = 1; i < n; i++) {
-        for (j = i - 1; j >= 0; j--) {
-            if (strcmp(string[j], string[j + 1]) == 1) {
-                char *buf = string[j];
-                string[j] = string[j + 1];
-                string[j + 1] = buf;
-}}}}
-
-void quicksort(char **string, int l, int r){
-    if (l >= r) return;
-    int pl = l, pr = r, m = (l + r) / 2;
-     do {
-        while (strcmp(string[m], string[pl]) == 1) pl++;
-        while (strcmp(string[m], string[pr]) == -1) pr--;
-        if (pl <= pr){
-        char *buf = string[pl];
-        string[pl] = string[pr];
-        string[pr] = buf;
-        pl++;
-        pr--;
-        }
-    } while (pl <= pr);
- quicksort (string, l, pr);
- quicksort (string, pl, r);
+void bubblesort(char **string, int n)
+{
+ int i, j;
+ for (i = 0; i < n; i++)
+   for (j = 0; j < n - i - 1; j++)
+     if (strcmp(string[j], string[j + 1]) == 1)
+      {
+       char *buf = string[j];
+       string[j] = string[j + 1];
+       string[j + 1] = buf;
+      }
 }
-void mergesort (char** string, int l, int r){
- if (l == r - 1) return;
- int m = (l + r) / 2, i;
- mergesort (string, l, m);
- mergesort (string, m, r);
- char** buf = malloc((r - l) * sizeof(char*));
- int pl = l, pr = m, newp = 0;
- while (pl < m && pr < r){
-    if (strcmp (string[pr], string[pl]) == 1) {
-        buf[newp] = string [pl];
-        newp++;
-        pl++;
+
+void insertionsort(char **string, int n)
+{
+ int i, j;
+ for (i = 1; i < n; i++)
+   for (j = i - 1; j >= 0; j--)
+     if (strcmp(string[j], string[j + 1]) == 1)
+      {
+       char *buf = string[j];
+       string[j] = string[j + 1];
+       string[j + 1] = buf;
+      }
+}
+
+void quicksort(char **string, int left, int right)
+{
+  if (left >= right) return;
+  int ptrleft = left, ptrright = right;
+  char* mid = string[(left + right) / 2];
+  while (ptrleft <= ptrright)
+   {
+    while (strcmp(mid, string[ptrleft]) == 1)
+      ptrleft++;
+    while (strcmp(mid, string[ptrright]) == -1)
+      ptrright--;
+    if (ptrleft <= ptrright)
+     {
+      char *buf = string[ptrleft];
+      string[ptrleft] = string[ptrright];
+      string[ptrright] = buf;
+      ptrleft++;
+      ptrright--;
+     }
+   }
+  if (left < ptrright)
+    quicksort (string, left, ptrright);
+  if (ptrleft < right)
+    quicksort (string, ptrleft, right);
+}
+
+void mergesort (char** string, int left, int right)
+{
+ if (left == right - 1) return;
+ int mid = (left + right) / 2;
+ int i;
+ mergesort (string, left, mid);
+ mergesort (string, mid, right);
+ char** buf = malloc((right - left) * sizeof(char*));
+ int ptrleft = left, ptrright = mid, newptr = 0;
+ while (ptrleft < mid && ptrright < right)
+   if (strcmp (string[ptrright], string[ptrleft]) == 1)
+    {
+     buf[newptr] = string [ptrleft];
+     newptr++;
+     ptrleft++;
     }
-    else {
-        buf[newp] = string [pr];
-        newp++;
-        pr++;
+   else
+    {
+     buf[newptr] = string [ptrright];
+     newptr++;
+     ptrright++;
     }
- }
- while (pl < m){
-    buf[newp] = string [pl];
-    newp++;
-    pl++;
- }
- while (pr < r){
-    buf[newp] = string [pr];
-    newp++;
-    pr++;
- }
-for (i = l; i < r; i++) string[i] = buf[i - l];
-free(buf);
+ while (ptrleft < mid)
+  {
+   buf[newptr] = string [ptrleft];
+   newptr++;
+   ptrleft++;
+  }
+ while (ptrright < right)
+  {
+   buf[newptr] = string [ptrright];
+   newptr++;
+   ptrright++;
+  }
+ for (i = left; i < right; i++)
+   string[i] = buf[i - left];
+ free(buf);
 }
 
-void heap (char** string, int i, int n){
-int max = i;
-while (1){
-    int child = 2 * i + 1;
-    if (child < n && strcmp(string[child], string[max]) == 1) max = child;
-    child++;
-    if (child < n && strcmp(string[child], string[max]) == 1) max = child;
-    if(max == i) break;
-    else {
-        char *buf = string[max];
-        string[max] = string[i];
-        string[i] = buf;
-        i = max;
+void heap (char** string, int i, int n)
+{
+ int max = i;
+ while (1)
+  {
+   int child = 2 * i + 1;
+   if (child < n && strcmp(string[child], string[max]) == 1)
+     max = child;
+   child++;
+   if (child < n && strcmp(string[child], string[max]) == 1)
+     max = child;
+   if(max == i) break;
+   else
+    {
+     char *buf = string[max];
+     string[max] = string[i];
+     string[i] = buf;
+     i = max;
     }
-}
-}
-
-void heapsort (char** string, int n){
-int i;
-for (i = n / 2; i >= 0; i--) heap(string, i, n);
-for (i = n - 1; i >= 1; i--){
-    char *buf = string[0];
-    string[0] = string[i];
-    string[i] = buf;
-    heap(string, 0, i);
-}
+  }
 }
 
+void heapsort (char** string, int n)
+{
+ int i;
+ for (i = n / 2; i >= 0; i--)
+   heap(string, i, n);
+ for (i = n - 1; i >= 1; i--)
+  {
+   char *buf = string[0];
+   string[0] = string[i];
+   string[i] = buf;
+   heap(string, 0, i);
+  }
+}
 
-int main (int argc, char* argv[]){
+
+int main (int argc, char* argv[])
+{
  FILE *file = fopen(argv[2], "r");
  int i, n;
  n = atoi(argv[1]);
- for (i = 0; i < n; ++i) {
-        string[i] = (char*) malloc(1025 * sizeof(char));
-        fgets(string[i], 1024, file);
- }
- switch (argv[3][0]) {
+ for (i = 0; i < n; ++i)
+  {
+   string[i] = (char*) malloc(1025 * sizeof(char));
+   fgets(string[i], 1024, file);
+  }
+ switch (argv[3][0])
+  {
         case 'b': {
             bubblesort(string, n);
             break;
@@ -133,9 +165,11 @@ int main (int argc, char* argv[]){
             break;
         }
         default: break;
- }
-for (i = 0; i < n; i++) printf("%s\n", string[i]);
-for (i = 0; i < n; i++) free (string[i]);
-fclose (file);
-return 0;
+  }
+ for (i = 0; i < n; i++)
+   printf("%s\n", string[i]);
+ for (i = 0; i < n; i++)
+   free (string[i]);
+ fclose (file);
+ return 0;
 }
