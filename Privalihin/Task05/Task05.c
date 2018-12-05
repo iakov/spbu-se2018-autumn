589ee353e2fb0976c7797bf7fa5a08c4680f7072
HappyLittleNurgling <Skorpilesha@yandex.ru>
2018-12-05 13:50:45 +0300

    Code style fixed

--- Privalihin/Task05/Task05.c
+++ Privalihin/Task05/Task05.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 void swapCStrings(char **a, char **b)
 {
