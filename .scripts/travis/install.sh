24d6392096e92d4891c558ceda1d5536869e1e7b
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-12-22 15:23:46 +0300

    [tests] new bash on TravisCI macOS

--- .scripts/travis/install.sh
+++ .scripts/travis/install.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    export HOMEBREW_NO_AUTO_UPDATE=1
+#    export HOMEBREW_NO_AUTO_UPDATE=1
 #for GNU's "readlink -f" and gawk with RS
-    brew install coreutils gawk gnu-sed
+#new bash for associative arrays
+    brew install coreutils gawk gnu-sed bash
 esac
