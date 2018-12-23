1ef5f59e631afc01def75f297d02a8924b86a4bf
Lev Malets <lk.malets30@gmail.com>
2018-12-23 10:09:42 +0300

    Fix file processing

--- Malets/Task05/utils.h
+++ Malets/Task05/utils.h
@@ -10,14 +10,14 @@ typedef enum
 
 typedef enum
 {
-    REASON_DESIRE_TO_DIE = 0,
+    REASON_NONE = 0,
     REASON_INVALID_ARGUMENTS = 1,
     REASON_NONEXISTENT_FILES = 2,
     REASON_INVALID_DATA = 3,
     REASON_SYSTEM_ERROR = 4,
 } Reason;
 
-extern void suicide( const char* suicideNote, Reason suicideReason );
+extern void forceExit( const char* suicideNote, Reason suicideReason );
 
 extern void* allocateMemory( uint64_t size );
 
