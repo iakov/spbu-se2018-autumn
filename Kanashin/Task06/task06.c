b99074b5f57a44fe40528a9284565548ac201ffc
Dmitry <dmitrii.a.kanashin@gmail.com>
2018-12-23 13:47:41 +0300

    task06 solution

---
+++ Kanashin/Task06/task06.c
@@ -0,0 +1,294 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "md5.h"
+
+#define MAX_SIZE_ARR 50
+#define MAX_WORD_LENGTH 100
+
+void checkErrors(char *errorMessage, uint32_t numberError)
+{
+    printf("%s", errorMessage);
+    exit(numberError);
+}
+
+/*void deletePunctuation(char *word)
+{
+    uint32_t currPosLetter;
+    char buffer[MAX_WORD_LENGTH];
+    uint32_t posWithoutPunct = 0;
+
+    for (currPosLetter = 0 ; currPosLetter <= strlen(word)-1; currPosLetter++)
+    {
+        buffer[currPosLetter] = word[currPosLetter];
+
+        if((buffer[currPosLetter] >= 'a' && buffer[currPosLetter] <= 'z') ||
+            (buffer[currPosLetter] >= 'A' && buffer[currPosLetter] <= 'Z') ||
+           ((buffer[currPosLetter] == '-' || (buffer[currPosLetter] == '\'') &&
+             currPosLetter != 0 && currPosLetter != strlen(word)-1)) &&
+           strlen(buffer) != 1)
+        {
+            word[posWithoutPunct] = tolower(buffer[currPosLetter]);
+            posWithoutPunct++;
+        }
+    }
+
+    word[posWithoutPunct] = '\0';
+
+    for (currPosLetter; currPosLetter > posWithoutPunct; currPosLetter--)
+    {
+        word[currPosLetter] = 0;
+    }
+}
+*/
+typedef struct HashList
+{
+    char* key;
+    uint32_t numberReplies;
+    struct HashList *ptrNext;
+}List;
+
+typedef struct
+{
+    List **lists;
+    uint32_t *lenghtList;
+    uint32_t sizeOfTable;
+
+}HashTable;
+
+
+HashTable newHashTable(uint32_t size)
+{
+    HashTable newTable;
+    newTable.lists = (List **) calloc(size, sizeof(List*));
+    if (newTable.lists == NULL)
+    {
+        checkErrors("Cannot allocate memory for creating hash table", 4);
+    }
+    newTable.sizeOfTable = size;
+    newTable.lenghtList = (uint32_t *) calloc(size, sizeof(uint32_t));
+    if (newTable.lenghtList == NULL)
+    {
+        checkErrors("Cannot allocate memory for counter of Length each of lists", 4);
+    }
+    return newTable;
+
+}
+
+uint32_t getHash(char *key)
+{
+    uint32_t hashRes;
+    uint32_t *res = (uint32_t *)calloc(4, sizeof(uint32_t));
+    if (res == NULL)
+    {
+        checkErrors("Cannot allocate memory for buffer of Hash", 4);
+    }
+    md5((uint8_t *)key, strlen(key), (uint8_t *)res);
+    hashRes = res[0];
+    free(res);
+    return hashRes;
+}
+
+List *findWord(HashTable *table, uint32_t index, char* word )
+{
+    List *currElemList;
+    currElemList = table->lists[index];
+    while (currElemList != NULL)
+    {
+        if (strcmp(currElemList->key , word) == 0)
+        {
+            return currElemList;
+        }
+        currElemList = currElemList->ptrNext;
+    }
+    return currElemList;
+}
+
+
+void addWord(char *word, HashTable *table, uint32_t numberOfReplies)
+{
+    uint32_t index = getHash(word) % table->sizeOfTable;
+    List *elemList;
+    elemList = findWord(table, index, word);
+    if (elemList != NULL)
+    {
+        elemList->numberReplies += numberOfReplies;
+    }
+    else
+    {
+        List *newElemList = malloc(sizeof(List));
+        if (newElemList == NULL)
+        {
+            checkErrors("Cannot allocate memory for adding new element of List", 4);
+        }
+
+        newElemList->key = (char *) malloc(strlen(word)*sizeof(uint32_t));
+        if (newElemList->key == NULL)
+        {
+            checkErrors("Cannot allocate memory for adding new word to the lists", 4);
+        }
+        strcpy(newElemList->key , word);
+        newElemList->numberReplies = numberOfReplies;
+
+        if (table->lists[index] == NULL)
+        {
+            newElemList->ptrNext = NULL;
+            table->lists[index] = newElemList;
+            table->lenghtList[index] = numberOfReplies;
+        }
+        else
+        {
+            newElemList->ptrNext = table->lists[index]->ptrNext;
+            table->lists[index]->ptrNext = newElemList;
+            table->lenghtList[index]++;
+        }
+    }
+
+}
+
+
+
+uint32_t getValueWord(HashTable *table, char* word)
+{
+    uint32_t index = getHash(word) % table->sizeOfTable;
+    List *elemList;
+    elemList = findWord(table, index, word);
+    if (elemList == NULL)
+    {
+        printf("There is no here current word\n");
+        return 0;
+    }
+
+    return elemList->numberReplies;
+
+}
+void getStat(HashTable *table)
+{
+    uint32_t maxNumberReplies = 0;
+    uint32_t avgLengthList = 0;
+    uint32_t amountLists = 0;
+    uint32_t amountWords = 0;
+    for (uint32_t i = 0; i < table->sizeOfTable; i++)
+    {
+        if (table->lenghtList[i] != 0)
+        {
+            avgLengthList += table->lenghtList[i];
+            amountLists++;
+        }
+
+        List *currElemList = table->lists[i];
+        while (currElemList != NULL)
+        {
+            uint32_t currNumberReplies = getValueWord(table, currElemList->key);
+            amountWords ++;
+            if (currNumberReplies > maxNumberReplies)
+            {
+                maxNumberReplies = currNumberReplies;
+            }
+            currElemList = currElemList->ptrNext;
+        }
+    }
+
+    for (uint32_t i = 0; i < table->sizeOfTable; i++)
+    {
+        List *currElemList = table->lists[i];
+        while (currElemList != NULL)
+        {
+            uint32_t currNumberReplies = getValueWord(table, currElemList->key);
+            if (currNumberReplies >= maxNumberReplies)
+            {
+                fprintf(stderr, "One of the most frequently words is \"%s\", appeared %d times\n", currElemList->key, currElemList->numberReplies);
+            }
+            currElemList = currElemList->ptrNext;
+        }
+    }
+    //printf("Total words are %d\n", amountWords);
+    //printf("Average length of Lists is %d", avgLengthList/amountLists);
+
+}
+void printWords(char *key, uint32_t numReplies)
+{
+    fprintf(stdout, "\"%s\" was appeared %d times.\n", key, numReplies);
+}
+void iterate(HashTable *table)
+{
+    for (uint32_t i = 0; i < table->sizeOfTable; i++)
+    {
+        List *currElemList = table->lists[i];
+        while (currElemList != NULL)
+        {
+            uint32_t currNumberReplies = getValueWord(table, currElemList->key);
+            printWords(currElemList->key, currNumberReplies);
+            currElemList = currElemList->ptrNext;
+        }
+    }
+}
+
+void deleteTable(HashTable *table)
+{
+    for (uint32_t i = 0; i < table->sizeOfTable; i++)
+    {
+        List *currElemList = table->lists[i];
+        while(currElemList != NULL)
+        {
+            List *buff = currElemList;
+            currElemList = currElemList->ptrNext;
+            free(buff->key);
+            free(buff);
+        }
+    }
+    free(table);
+}
+
+int main()
+{
+
+    uint32_t numberOfReplies = 1;
+    char buffer[MAX_WORD_LENGTH];
+    HashTable table = newHashTable(MAX_SIZE_ARR);
+    char symb;
+    char *word;
+    uint32_t posWithoutPunct;
+
+    while ((symb = getchar()) != EOF)
+    {
+        if((symb >= 'a' && symb <= 'z') ||
+            (symb >= 'A' && symb <= 'Z') ||
+            ((symb == '-') || (symb == '\'')))
+        {
+            buffer[posWithoutPunct] = tolower(symb);
+            posWithoutPunct++;
+        }
+        else if(posWithoutPunct > 0)
+        {
+            while (buffer[posWithoutPunct - 1] == '-' || buffer[posWithoutPunct - 1] == '\'' )
+            {
+                buffer[posWithoutPunct--] = '\0';
+            }
+
+            buffer[posWithoutPunct] = '\0';
+
+            word = (char *)calloc(strlen(word) + 1, sizeof(char));
+            if (word == NULL)
+            {
+                checkErrors("Cannot allocate memory for reading word", 4);
+            }
+
+            strcpy(word, buffer);
+
+            addWord(word, &table, numberOfReplies);
+
+            posWithoutPunct = 0;
+            free(word);
+        }
+
+    }
+
+    iterate(&table);
+    getStat(&table);
+    deleteTable(&table);
+
+
+    return 0;
+}
