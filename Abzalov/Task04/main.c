5f4d83a0ad936b40353ffe9ea2c366fb0b441952
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2018-12-23 04:37:45 -0500

    Added Task04

---
+++ Abzalov/Task04/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define igr(x) {__typeof__(x) __attribute__((unused)) d=(x);} 
+
+
+void other() {
+    printf("Congratulations!\n");
+    printf("You have entered in the secret function!\n");
+}
+
+void input() {
+    char buffer[20];
+
+    printf("Enter some text:\n");
+    if (scanf("%s", buffer) == 1){};
+    printf("You entered: %s\n", buffer);
+}
+
+int main() {
+    input();
+    return 0;
+}
