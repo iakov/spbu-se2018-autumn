231e395ebc3c8dab8a306bec8ce3f42535c4c3b2
Dmitry <dmitrii.a.kanashin@gmail.com>
2018-12-23 14:16:49 +0300

    fixed errors with md5.h

--- Kanashin/Task06/md5.h
+++ Kanashin/Task06/md5.h
@@ -1,7 +1,10 @@
 #ifndef MD5_H
 #define MD5_H
 
-#include "md5.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
 
